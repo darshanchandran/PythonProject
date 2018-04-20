adba78241d6856916347a864780eeecc601259fe
darshanchandran <darshansa@gmail.com>
2018-04-20 17:54:46 +0530

    file regex added

--- PythonProject/Python programming/RegularExpressions/fileregex.py
+++ PythonProject/Python programming/RegularExpressions/fileregex.py
@@ -1,2 +1,23 @@
-#This program shows how regex can be used
-#This is Dev file
+# This program shows how regex can be used reading through file
+
+import re
+
+
+class Readfile(object):
+
+    def openfile(self):
+        file = input("Enter the File to be read : ")
+        openitem = open(file, 'r')
+        readitem = openitem.read()
+        # print(openitem.read())
+
+        matches = re.findall(r'\d{1}', readitem)
+        print(matches)
+        openitem.close()
+
+
+read = Readfile()
+
+read.openfile()
+
+
